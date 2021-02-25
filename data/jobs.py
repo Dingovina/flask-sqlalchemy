dac1ff20bc9536d17cdc0e9577c177ccc8bb376b
Dingovina <65973711+Dingovina@users.noreply.github.com>
2021-02-25 19:02:54 +0500

    task1 + new job in db = task2

--- data/jobs.py
+++ data/jobs.py
@@ -15,7 +15,7 @@ class Jobs(SqlAlchemyBase):
     collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now, nullable=True)
     end_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now, nullable=True)
-    is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
 
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
--- main.py
+++ main.py
@@ -14,48 +14,16 @@ def main():
     db_session.global_init("db/blogs.db")
     db_sess = db_session.create_session()
 
-    user = User()
-    user.surname = 'Scott'
-    user.name = 'Ridley'
-    user.age = 21
-    user.position = 'captain'
-    user.speciality = 'research engineer'
-    user.address = 'module_1'
-    user.email = 'scott_chief@mars.org'
-    db_sess.add(user)
-    db_sess.commit()
-
-    user = User()
-    user.surname = 'John'
-    user.name = 'Rain'
-    user.age = 32
-    user.position = 'lowest'
-    user.speciality = 'sleeper'
-    user.address = 'module_2'
-    user.email = 'john_rain@mars.org'
-    db_sess.add(user)
-    db_sess.commit()
-
-    user = User()
-    user.surname = 'Andy'
-    user.name = 'Ydna'
-    user.age = 22
-    user.position = 'cap helper'
-    user.speciality = 'space scientist'
-    user.address = 'module_1'
-    user.email = 'best_andy123@mars.org'
-    db_sess.add(user)
-    db_sess.commit()
-
-    user = User()
-    user.surname = 'Karl'
-    user.name = 'Ltak'
-    user.age = 19
-    user.position = 'lowest'
-    user.speciality = 'ship cleaner'
-    user.address = 'module_9'
-    user.email = 'ilovemywork@mars.org'
-    db_sess.add(user)
+    user = db_sess.query(User).filter(User.id == 1).first()
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.start_date = datetime.datetime.now()
+    job.is_finished = False
+    job.user = user
+    db_sess.add(job)
     db_sess.commit()
 
 
